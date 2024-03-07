2c9c7c9fcfc14ca6c02774899cb64af19d9d3ac5
Sandro <84281442+SandroSuknidze@users.noreply.github.com>
2024-03-07 21:59:51 +0400

    Changed BaseUrl

--- src/utils/axios-instance.js
+++ src/utils/axios-instance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL: 'http://188.166.106.66/api',
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://188.166.106.66/api',
+    // baseURL: 'http://localhost:8000/api',
     timeout: 1000,
     headers: {
         'Content-Type': 'application/json',
